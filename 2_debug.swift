2b6746fd4c8d92b270cdd8e10d4db3db0381676f
Masayoshi Asakura <asache3@gmail.com>
2015-05-28 08:28:46 +0900

    Add 2_debug

---
+++ 2_debug.swift
@@ -0,0 +1,15 @@
+let msg = "ごきげんよう"
+let num1 = 12345
+let array = [1, 2, 3, 4, 5]
+println(msg)
+print("数字は\(num1)です。")
+println("-----")
+println(array)
+        
+println(__FILE__)
+println(__LINE__)
+println(__COLUMN__)
+println(__FUNCTION__)
+        
+var num2 = 0
+assert(num2 != 0, "num2は0であってはいけない")
