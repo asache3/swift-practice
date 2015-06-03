6665ab46f6d03397e30be74ba2627e605fdc9369
Masayoshi Asakura <asache3@gmail.com>
2015-06-04 07:38:35 +0900

    Delete some lines

--- 2_debug.swift
+++ 2_debug.swift
@@ -5,11 +5,4 @@ println(msg)
 print("数字は\(num1)です。")
 println("-----")
 println(array)
-        
-println(__FILE__)
-println(__LINE__)
-println(__COLUMN__)
-println(__FUNCTION__)
-        
-var num2 = 0
-assert(num2 != 0, "num2は0であってはいけない")
+
