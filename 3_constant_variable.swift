d2538d1a79d857aa336b07676c7773cb6bf9846d
Masayoshi Asakura <asache3@gmail.com>
2015-06-04 07:58:46 +0900

    Add comment

--- 3_constant_variable.swift
+++ 3_constant_variable.swift
@@ -1,17 +1,20 @@
 import UIKit
 
-var tax8:Double = 0.08
+// 定数 
+let tax8:Double = 0.08
 let theLanguage:String = "Swift"
 let now = NSDate()
 let logoCOLOR = UIColor(red:0.16, green:0.73, blue:0.61, alpha:1)
 let mens:Int = 100, womens:Int = 200
 
+// 変数
 var theYear:Int = 2014
 var id:String, size:Double
 theYear = 1999
 id = "123A"
 size = 23.5
 
+// 型キャスト
 let tax = 0.08
 var tanka = 120
 var kosu = 3
