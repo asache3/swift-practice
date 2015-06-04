fc33f8314fa0cb09c2257742ea488eb4a8afcfa5
Masayoshi Asakura <asache3@gmail.com>
2015-06-04 22:33:17 +0900

    Modify 3_constant

--- 3_constant_variable.swift
+++ 3_constant_variable.swift
@@ -1,11 +1,14 @@
 import UIKit
 
 // 定数 
-let tax8:Double = 0.08
-let theLanguage:String = "Swift"
+let tax1 = 0.08
+let tax2:Double = 0.08
+let theLanguage1 = "Swift"
+let theLanguage2:String = "Swift"
 let now = NSDate()
 let logoCOLOR = UIColor(red:0.16, green:0.73, blue:0.61, alpha:1)
-let mens:Int = 100, womens:Int = 200
+let mens1 = 100, womens1 = 200
+let mens2:Int = 100, womens2:Int = 200
 
 // 変数
 var theYear:Int = 2014
