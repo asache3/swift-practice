e8b95496f863af32dbbaf65321ae7c6a787d7bcd
Masayoshi Asakura <asache3@gmail.com>
2015-06-14 23:31:59 +0900

    Add 18_struct

---
+++ 18_struct.swift
@@ -0,0 +1,92 @@
+// 構造体 struct
+struct ColorBox {
+    var width:Int
+    var height:Int
+    var color:String
+}
+
+struct WhiteBox {
+    var width:Int = 100
+    var height:Int = 100
+    let color:String = "white"
+}
+
+var redBox = ColorBox(width: 120, height: 100, color: "red")
+var theBox = WhiteBox()
+println((redBox.width, redBox.height, redBox.color))
+println((theBox.width, theBox.height, theBox.color))
+
+redBox.width = 90
+redBox.color = "blue"
+println("幅\(redBox.width)、高さ\(redBox.height)、色\(redBox.color)")
+
+
+// イニシャライザのある構造体
+struct Box {
+    let width:Int
+    let height:Int
+    let size:String
+    
+    init(width:Int, height:Int) {
+        self.width = width
+        self.height = height
+        
+        if(width+height)<250 {
+            size = "M"
+        } else {
+            size = "L"
+        }
+    }
+}
+
+let box1 = Box(width: 120, height: 80)
+let box2 = Box(width: 150, height: 120)
+println("box1のサイズは\(box1.size)")
+println("box2のサイズは\(box2.size)")
+
+
+// 関数がある構造体
+struct Lot {
+    var kosu:Int
+    var tanka:Int
+    
+    func price() -> Int {
+        return kosu * tanka
+    }
+}
+
+let lot4 = Lot(kosu: 4, tanka: 780)
+let price4 = lot4.price()
+println("4個入り\(price4)円")
+
+
+// subscriptを定義した構造体
+struct Stock {
+    var name:String
+    var data:[String:Int] = [:]
+    
+    init(name:String) {
+        self.name = name
+    }
+    
+    subscript(color:String, size:Double) -> Int {
+        get {
+            let key = color + size.description
+            if let value = data[key] {
+                return value
+            } else {
+                return 0
+            }
+        }
+        
+        set {
+            let key = color + size.description
+            data[key] = newValue
+        }
+    }
+}
+
+var addixStock = Stock(name: "addix")
+addixStock["green", 25.0] = 3
+addixStock["green", 25.0] += 1
+println(addixStock["green", 25.0])
