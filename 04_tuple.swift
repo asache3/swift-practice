a5c316a6b0753622190a9a2e0e21bc7ef8c03d08
Masayoshi Asakura <asache3@gmail.com>
2015-06-15 09:01:22 +0900

    Modify variable name and comment

--- 04_tuple.swift
+++ 04_tuple.swift
@@ -12,9 +12,9 @@ println(price2)
 
 
 // タプルの値にインデックス番号でアクセスする
-let kingaku3 = (1000, 80)
-let price3 = kingaku3.0
-let tax3 = kingaku3.1
+let kingaku1 = (1000, 80)
+let price3 = kingaku1.0
+let tax3 = kingaku1.1
 println(price3 + tax3)
 
 var user1 = ("鈴木", 29)
@@ -23,12 +23,12 @@ println(user1)
 
 
 // ラベル付きのタプルを作る
-let kingaku4 = (price4:100, tax4:80)
-let seikyugaku = kingaku4.price4 + kingaku4.tax4
+let kingaku2 = (price4:100, tax4:80)
+let seikyugaku = kingaku2.price4 + kingaku2.tax4
 println(seikyugaku)
 
 
-// タプルを値に持つ変数を宣言する
+// ラベルと型を指定したタプルを作る
 var user2:(name:String, age:Int, isPass:Bool)
 user2.name = "高田"
 user2.age = 23
